7d7969c8169fb86e9faea56212edb00ab14b94e2
Tom Ramee <tramee@vps118743.vps.ovh.ca>
2017-03-05 02:42:05 -0800

    Working version with CSFR token in the POST form

--- __init__.py
+++ __init__.py
@@ -1,32 +1,53 @@
-#from OpenSSL import SSL
-from flask import Flask, render_template, jsonify, flash
+from flask import Flask, render_template, redirect, url_for
+from os import urandom
 from module1 import *
 
-#context = SSL.Context(SSL.SSLv23_METHOD)
-#context.use_privatekey_file('/etc/apache2/ssl/apache.key')
-#context.use_certificate_file('/etc/apache2/ssl/apache.crt')
 
 app = Flask(__name__)
+app.config['CSRF_ENABLED'] = True
 
 @app.route('/')
 def homepage():
     return 'Site in construction; come again soon.'
 
-@app.route('/form/', methods=['GET','POST'])    
+@app.route('/result/')
+def result():
+    return render_template('result.html')
+
+@app.route('/rollers/')
+def rollers_summary():
+    return render_template('rollers.html',roller=None)
+
+@app.route('/rollers/<int:r_id>')
+def rollers(r_id):
+    if r_id not in [24, 25]:
+        r_id = None
+    return render_template('rollers.html',roller=r_id)
+
+@app.route('/form/', methods=['GET','POST'])
 def renderForm():
     form = testForm()
-    if form.validate_on_submit():
-        status = {'status': 'SUCCESS',
-                  'answer': form.answer.data,
-                  'other': form.other.data}
-        return render_template('result.html', data=status)
+    status = False
+    if form.is_submitted():
+        print "submitted"
+    if form.validate():
+        print "valid"
+        status = True
+    else:
+        print (form.errors)
+
+    if status:
+        # status = {'status': 'SUCCESS',
+        #           'answer': form.answer.data,
+        #           'other': form.other.data}
+        return redirect(url_for('result'))
     try: 
         return render_template('form.html', form=form)
     except Exception as e:
         return str(e)
     
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
     #app.run(ssl_context=context)
     #app.run(host='0.0.0.0', ssl_context=context)
     
--- module1.py
+++ module1.py
@@ -1,11 +1,10 @@
-#from flask_wtf import Form
-from flask_wtf import FlaskForm as Form
+from flask_wtf import FlaskForm
 from wtforms.fields import RadioField, SubmitField
 
-class testForm(Form):
+class testForm(FlaskForm):
     answer =RadioField('First field',
                         choices=[('yes','Yes'),('no','No')])
     other = RadioField('Second field',
-                        choices=[(1,'Pink'),(2,'Blue'),(3,'Black')])
+                        choices=[('1','Pink'),('2','Blue'),('3','Black')])
     submit = SubmitField('Submit')
 
